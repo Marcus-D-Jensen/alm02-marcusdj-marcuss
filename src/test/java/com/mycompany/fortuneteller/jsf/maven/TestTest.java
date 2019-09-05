b93266f5e215c69dee7e66d54b3233f8b6cb0d72
Marcus D Jensen <marcus8209@gmail.com>
2019-09-05 13:21:34 +0200

    first commit

---
+++ src/test/java/com/mycompany/fortuneteller/jsf/maven/TestTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.fortuneteller.jsf.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		//	fail("Not yet implemented");
+	}
+
+}
